711910902e576b1f197f8ef8d81a17d5431d2e07
yuya-h-29 <tzou11291996@gmail.com>
2020-06-07 08:36:07 +0900

    upload photo added functionality

--- lib/pages/uploadphoto.dart
+++ lib/pages/uploadphoto.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
 class UploadPhoto extends StatefulWidget {
   @override
@@ -8,6 +11,16 @@ class UploadPhoto extends StatefulWidget {
 }
 
 class _UploadPhotoState extends State<UploadPhoto> {
+  File _image;
+
+  Future getImage() async {
+    final image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +34,32 @@ class _UploadPhotoState extends State<UploadPhoto> {
                 'Upload Picture!',
                 style: TextStyle(fontSize: 28),
               ),
-              Image.asset('images/blank.jpg'),
+              Container(
+                child: _image == null
+                    ? Image.asset('images/blank.jpg')
+                    : Image.file(_image),
+              ),
               RaisedButton(
-                child: Text('Upload',
-                    style: TextStyle(
-                      color: Colors.grey[900],
-                      fontWeight: FontWeight.bold,
-                    )),
+                child: _image == null
+                    ? Text(
+                        'Upload',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    : Text(
+                        'Next',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                 color: Colors.pinkAccent,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
-                onPressed: () => {},
+                onPressed: () => {getImage()},
               ),
             ],
           ),
