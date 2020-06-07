ce02386c545180c07f2c4be0309d7d6d149c7a1f
Yurika Namba <yurikanamba@gmail.com>
2020-06-07 15:26:53 +0900

    saving before pull

--- lib/pages/createProfile.dart
+++ lib/pages/createProfile.dart
@@ -1,19 +1,31 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:chips_choice/chips_choice.dart';
-
-
 import 'package:flutter/foundation.dart';
+import "package:lovealapp/services/auth.dart";
+import 'package:lovealapp/shared/loading.dart';
 
 class CreateProfile extends StatefulWidget {
-
   @override
   _CreateProfileState createState() => _CreateProfileState();
 }
 
 class _CreateProfileState extends State<CreateProfile> {
-  String age = '20 - 29';
-  String gender = 'Female';
+  final AuthService _auth = AuthService();
+
+  String nickname;
+  String location;
+  String age;
+  String gender;
+  String occupation;
+  String about;
+  String interests;
+
+  //for loading spinner
+  bool loading = false;
+
+//  String age = '20 - 29';
+//  String gender = 'Female';
 //  List<bool> isSelected;
 //
 //  @override
@@ -34,86 +46,99 @@ class _CreateProfileState extends State<CreateProfile> {
 //  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Choose a nickname to display to users'
-                ),
-                keyboardType: TextInputType.text,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter your Location'
-                ),
-                keyboardType: TextInputType.text,
-              ),
-              Row(
-                children: <Widget>[
-                  Text(
-                    'Age'
-                  ),
-                  DropdownButton<String>(
-                    value: age,
-                    iconSize: 24,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        age = newValue;
-                      });
-                    },
-                    items: <String>['20 - 29', '30 - 39', '40 - 49', '50 - 59', '60 - 69']
-                        .map<DropdownMenuItem<String>>(
-                            (String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                  ),
-                  Text(
-                    "Gender",
-                  ),
-                  DropdownButton<String>(
-                    value: gender,
-                    iconSize: 24,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        gender = newValue;
-                      });
-                    },
-                    items: <String>['Female', 'Male', 'Other']
-                        .map<DropdownMenuItem<String>>(
-                            (String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                  ),
-                ],
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Enter your occupation'
-                ),
-                keyboardType: TextInputType.text,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Share something about yourself'
-                ),
-                keyboardType: TextInputType.text,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'What are your interests?'
-                ),
-                keyboardType: TextInputType.text,
-              )
+    return loading
+        ? Loading()
+        : Scaffold(
+            body: SafeArea(
+              child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        onChanged: (val) {
+                          setState(() => nickname = val);
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Choose a nickname to display to users'),
+                        keyboardType: TextInputType.text,
+                      ),
+                      TextField(
+                        onChanged: (val) {
+                          setState(() => location = val);
+                        },
+                        decoration:
+                            InputDecoration(labelText: 'Enter your Location'),
+                        keyboardType: TextInputType.text,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text('Age'),
+                          DropdownButton<String>(
+                            value: age,
+                            iconSize: 24,
+                            onChanged: (String newValue) {
+                              setState(() {
+                                age = newValue;
+                              });
+                            },
+                            items: <String>[
+                              '20 - 29',
+                              '30 - 39',
+                              '40 - 49',
+                              '50 - 59',
+                              '60 - 69'
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                          Text(
+                            "Gender",
+                          ),
+                          DropdownButton<String>(
+                            value: gender,
+                            iconSize: 24,
+                            onChanged: (String newValue) {
+                              setState(() {
+                                gender = newValue;
+                              });
+                            },
+                            items: <String>['Female', 'Male', 'Other']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                      TextField(
+                        onChanged: (val) {
+                          setState(() => occupation = val);
+                        },
+                        decoration:
+                            InputDecoration(labelText: 'Enter your occupation'),
+                        keyboardType: TextInputType.text,
+                      ),
+                      TextField(
+                        onChanged: (val) {
+                          setState(() => about = val);
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Share something about yourself'),
+                        keyboardType: TextInputType.text,
+                      ),
+                      TextField(
+                        onChanged: (val) {
+                          setState(() => interests = val);
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'What are your interests?'),
+                        keyboardType: TextInputType.text,
+                      )
 //          ToggleButtons(
 //            children: <Widget>[
 //              Text("murder"),
@@ -134,10 +159,9 @@ class _CreateProfileState extends State<CreateProfile> {
 //            },
 //            isSelected: isSelected,
 //          ),
-                ],
-              )
-              ),
-          ),
-    );
+                    ],
+                  )),
+            ),
+          );
   }
 }
