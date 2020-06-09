04c25d4c1074978c8c434c0dd1a05b5b5141640d
Kapakahi <rubbishman@gmail.com>
2020-06-09 12:15:19 +0900

    got Google social login working

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:lovealapp/models/user.dart";
 import 'package:lovealapp/services/database.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -62,7 +63,27 @@ class AuthService {
     }
   }
 
-  // reset password
+  //Login with The Google
+  Future loginWithGoogle() async {
+    try {
+      GoogleSignIn googleSignIn = GoogleSignIn();
+      GoogleSignInAccount account = await googleSignIn.signIn();
+      if(account == null)
+        return false;
+      AuthResult res = await _auth.signInWithCredential(GoogleAuthProvider.getCredential(
+          idToken: (await account.authentication).idToken,
+          accessToken: (await account.authentication).accessToken,
+      ));
+      if(res.user == null)
+        return false;
+      return true;
+    } catch (e) {
+      print("Error logging with Google");
+      return false;
+    }
+  }
+
+  // reset password (NOT NEEDED?!?)
   Future sendPasswordResetEmail(String email) async {
     return await _auth.sendPasswordResetEmail(email: email);
   }
