2b690f5ac8697d75f589bb5706ebac0c04ead86f
Yurika Namba <yurikanamba@gmail.com>
2020-06-11 17:58:57 +0900

    completed limit matches logic I think

--- lib/main.dart
+++ lib/main.dart
@@ -30,22 +30,19 @@ class MyApp extends StatelessWidget {
           title: 'Loveal',
           debugShowCheckedModeBanner: false,
           theme: ThemeData.light(),
-          home: CreateProfile(),
-
 // this goes under theme
-//          initialRoute: '/',
-//          routes: {
-//            '/': (_) => Wrapper(),
-//            '/signup': (_) => SignUp(),
-//            '/login': (_) => Login(),
-//            '/forgotpassword': (_) => ForgotPassword(),
-//            '/createProfile': (_) => CreateProfile(),
-//            '/uploadphoto': (_) => UploadPhoto(),
-//            '/editProfile': (_) => EditProfile(),
-//            '/getMatch': (_) => GetMatch(),
-//            '/navigationHome': (_) => NavigationHome(),
-//          }
-          ),
+          initialRoute: '/',
+          routes: {
+            '/': (_) => Wrapper(),
+            '/signup': (_) => SignUp(),
+            '/login': (_) => Login(),
+            '/forgotpassword': (_) => ForgotPassword(),
+            '/createProfile': (_) => CreateProfile(),
+            '/uploadphoto': (_) => UploadPhoto(),
+            '/editProfile': (_) => EditProfile(),
+            '/getMatch': (_) => GetMatch(),
+            '/navigationHome': (_) => NavigationHome(),
+          }),
     );
   }
 }
