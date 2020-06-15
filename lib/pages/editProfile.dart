88a574f65517797f8bb1bef8b70e049c81bed6f3
Kapakahi <rubbishman@gmail.com>
2020-06-15 17:30:03 +0900

    pulling

--- lib/pages/editProfile.dart
+++ lib/pages/editProfile.dart
@@ -19,10 +19,10 @@ class _EditProfileState extends State<EditProfile> {
 
   String nickname;
   String location;
-  String age = '20 - 29';
+  String age;
   String gender = 'Female';
   String occupation;
-  String about;
+  String about;    
 
   //for interests
   bool yodeling = false;
@@ -190,20 +190,10 @@ class _EditProfileState extends State<EditProfile> {
                                                         });
                                                       },
                                                       items: <String>[
-                                                        '20 - 29',
-                                                        '30 - 39',
-                                                        '40 - 49',
-                                                        '50 - 59',
-                                                        '60 - 69',
-                                                        '70 - 79',
-                                                        '80 - 89',
-                                                        '90 - 99'
-                                                      ].map<
-                                                              DropdownMenuItem<
-                                                                  String>>(
+                                                        '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80'
+                                                      ].map<DropdownMenuItem<String>>(
                                                           (String value) {
-                                                        return DropdownMenuItem<
-                                                            String>(
+                                                        return DropdownMenuItem<String>(
                                                           value: value,
                                                           child: Text(value),
                                                         );
