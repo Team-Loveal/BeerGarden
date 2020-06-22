4567aa7e22bbe9d4f05a6a4fa555551a8a235d29
Jeff Bentley <jbentley449@gmail.com>
2020-06-22 20:18:29 +0900

    added blur to all the images, still the full size image is a little strange with the blur

--- lib/widgets/fullScreenImage.dart
+++ lib/widgets/fullScreenImage.dart
@@ -12,7 +12,7 @@ Widget fullScreenImage(
             Image(image: NetworkImage(src)),
             Positioned.fill(
               child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+                  filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
                   child: Container(color: Colors.black.withOpacity(0))),
             ),
           ],
