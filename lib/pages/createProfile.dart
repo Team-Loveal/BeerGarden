cd7e7c3baa7909767ab544c16ac4cad9069c5924
yuya-h-29 <tzou11291996@gmail.com>
2020-06-22 17:35:23 +0900

    pull

--- lib/pages/createProfile.dart
+++ lib/pages/createProfile.dart
@@ -136,125 +136,129 @@ class _CreateProfileState extends State<CreateProfile> {
                                 ),
                                 Padding(
                                   padding: const EdgeInsets.all(5.0),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      Text('Age'),
-                                      Expanded(
-                                        child: DropdownButtonFormField<int>(
-                                          validator: (value) => value == null
-                                              ? 'Please tell us your age'
-                                              : null,
-                                          value: age,
-                                          iconSize: 24,
-                                          onChanged: (int newValue) {
-                                            setState(() {
-                                              age = newValue;
-                                            });
-                                          },
-                                          items: <int>[
-                                            18,
-                                            19,
-                                            20,
-                                            21,
-                                            22,
-                                            23,
-                                            24,
-                                            25,
-                                            26,
-                                            27,
-                                            28,
-                                            29,
-                                            30,
-                                            31,
-                                            32,
-                                            33,
-                                            34,
-                                            35,
-                                            36,
-                                            37,
-                                            38,
-                                            39,
-                                            40,
-                                            41,
-                                            42,
-                                            43,
-                                            44,
-                                            45,
-                                            46,
-                                            47,
-                                            48,
-                                            49,
-                                            50,
-                                            51,
-                                            52,
-                                            53,
-                                            54,
-                                            55,
-                                            56,
-                                            57,
-                                            58,
-                                            59,
-                                            60,
-                                            61,
-                                            62,
-                                            63,
-                                            64,
-                                            65,
-                                            66,
-                                            67,
-                                            68,
-                                            69,
-                                            70,
-                                            71,
-                                            72,
-                                            73,
-                                            74,
-                                            75,
-                                            76,
-                                            77,
-                                            78,
-                                            79,
-                                            80
-                                          ].map<DropdownMenuItem<int>>(
-                                              (int value) {
-                                            return DropdownMenuItem<int>(
-                                              value: value,
-                                              child: Text(value.toString()),
-                                            );
-                                          }).toList(),
+                                  child: Container(
+//                                    color: Colors.white,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+
+                                        Text('Age'),
+                                        Expanded(
+                                          child: DropdownButtonFormField<int>(
+                                            validator: (value) => value == null
+                                                ? 'Please tell us your age'
+                                                : null,
+                                            value: age,
+                                            iconSize: 24,
+                                            onChanged: (int newValue) {
+                                              setState(() {
+                                                age = newValue;
+                                              });
+                                            },
+                                            items: <int>[
+                                              18,
+                                              19,
+                                              20,
+                                              21,
+                                              22,
+                                              23,
+                                              24,
+                                              25,
+                                              26,
+                                              27,
+                                              28,
+                                              29,
+                                              30,
+                                              31,
+                                              32,
+                                              33,
+                                              34,
+                                              35,
+                                              36,
+                                              37,
+                                              38,
+                                              39,
+                                              40,
+                                              41,
+                                              42,
+                                              43,
+                                              44,
+                                              45,
+                                              46,
+                                              47,
+                                              48,
+                                              49,
+                                              50,
+                                              51,
+                                              52,
+                                              53,
+                                              54,
+                                              55,
+                                              56,
+                                              57,
+                                              58,
+                                              59,
+                                              60,
+                                              61,
+                                              62,
+                                              63,
+                                              64,
+                                              65,
+                                              66,
+                                              67,
+                                              68,
+                                              69,
+                                              70,
+                                              71,
+                                              72,
+                                              73,
+                                              74,
+                                              75,
+                                              76,
+                                              77,
+                                              78,
+                                              79,
+                                              80
+                                            ].map<DropdownMenuItem<int>>(
+                                                (int value) {
+                                              return DropdownMenuItem<int>(
+                                                value: value,
+                                                child: Text(value.toString()),
+                                              );
+                                            }).toList(),
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        "Gender",
-                                      ),
-                                      Expanded(
-                                        child: DropdownButtonFormField<String>(
-                                          validator: (value) => value == null
-                                              ? 'Please tell us your gender'
-                                              : null,
-                                          value: gender,
-                                          iconSize: 24,
-                                          onChanged: (String newValue) {
-                                            setState(() {
-                                              gender = newValue;
-                                            });
-                                          },
-                                          items: <String>[
-                                            'Female',
-                                            'Male',
-                                            'Rather not say'
-                                          ].map<DropdownMenuItem<String>>(
-                                              (String value) {
-                                            return DropdownMenuItem<String>(
-                                              value: value,
-                                              child: Text(value),
-                                            );
-                                          }).toList(),
+                                        Text(
+                                          "Gender",
                                         ),
-                                      ),
-                                    ],
+                                        Expanded(
+                                          child: DropdownButtonFormField<String>(
+                                            validator: (value) => value == null
+                                                ? 'Please tell us your gender'
+                                                : null,
+                                            value: gender,
+                                            iconSize: 24,
+                                            onChanged: (String newValue) {
+                                              setState(() {
+                                                gender = newValue;
+                                              });
+                                            },
+                                            items: <String>[
+                                              'Female',
+                                              'Male',
+                                              'Rather not say'
+                                            ].map<DropdownMenuItem<String>>(
+                                                (String value) {
+                                              return DropdownMenuItem<String>(
+                                                value: value,
+                                                child: Text(value),
+                                              );
+                                            }).toList(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                                 Padding(
