5c93805cf44a1341b7228f3ba978501ee00f291f
Yurika Namba <yurikanamba@gmail.com>
2020-06-23 11:04:07 +0900

    saving

--- lib/widgets/fullScreenImage.dart
+++ lib/widgets/fullScreenImage.dart
@@ -5,16 +5,25 @@ Widget fullScreenImage(
     BuildContext context, String src, double sigmaX, sigmaY) {
   return GestureDetector(
     child: Center(
-      child: Hero(
-        tag: 'imageHero',
+      child: Container(
+        //padding: EdgeInsets.all(10),
+        //alignment: Alignment.centerRight,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        //tag: 'imageHero',
         child: Stack(
           children: <Widget>[
             Image(image: NetworkImage(src)),
             Positioned.fill(
+                //Container(
+                //width: MediaQuery.of(context).size.width,
+                //height: MediaQuery.of(context).size.height,
+                child: ClipRRect(
               child: BackdropFilter(
                   filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
                   child: Container(color: Colors.black.withOpacity(0))),
-            ),
+            )),
+            //),
           ],
         ),
       ),
